95186d0b48772cf9fcdec0561ab50fbc4a901156
Yubraj Bhandari <yubraj.bhandari.hero@gmail.com>
2022-05-01 18:39:35 +0545

    Working on bot

--- myscript.py
+++ myscript.py
@@ -5,7 +5,10 @@ with open('Ambulnace.json', encoding='utf-8') as f:
     j = json.loads(f.read())
     for i in range(100):
         a = j[i]
-        d= Ambulance.objects.create(name=a["name"], description=a.get("description", "N/A"), hospital_name=a.get("hospital_name", "N/A"), phone=int(a.get("phone", "0")), image=a.get("image", "N/A"))
-        d.save()
+        try:
+            d= Ambulance.objects.create(name=a["name"], description=a.get("description", "N/A"), hospital_name=a.get("hospital_name", "N/A"), phone=a.get("phone", "0"), image=a.get("image", "N/A"))
+            d.save()
+        except Exception:
+            continue
         print(f"Created {d} ")
 
